91b7554c74f15148647865860e9ce3c8bb225b8d
FocusTorn <slettenmd@gmail.com>
2025-07-20 06:39:25 -0500

    WIP: (GW, PB) Single build tooling

--- packages/_utilities/src/index.ts
+++ packages/_utilities/src/index.ts
@@ -1,18 +1,13 @@
-// Individual exports for manual tree-shaking
-// Interfaces
-export type { IConfigurationService } from './_interfaces/IConfigurationService.js'
-export type { IFileSystem, FileStats, DirectoryEntry } from './_interfaces/IFileSystem.js'
-export type { IProcess } from './_interfaces/IProcess.js'
-
-// Services
+// Barrel exports
+export * from './_interfaces/IConfigurationService.js'
+export * from './_interfaces/IFileSystem.js'
+export * from './_interfaces/IProcess.js'
 export { ConfigurationService } from './services/Configuration.service.js'
-
-// VSCode Utilities
 export { showTimedInformationMessage } from './vscode/window.js'
 
-// Legacy barrel exports (for backward compatibility)
-// export * from './_interfaces/IConfigurationService.js'
-// export * from './_interfaces/IFileSystem.js'
-// export * from './_interfaces/IProcess.js'
+// Individual exports for manual tree-shaking (commented out for now)
+// export type { IConfigurationService } from './_interfaces/IConfigurationService.js'
+// export type { IFileSystem, FileStats, DirectoryEntry } from './_interfaces/IFileSystem.js'
+// export type { IProcess } from './_interfaces/IProcess.js'
 // export { ConfigurationService } from './services/Configuration.service.js'
 // export { showTimedInformationMessage } from './vscode/window.js'
